750d62928ffec8bf8d106744e03f30bbb88a9d9d
Alex745i <alex745i@gmail.com>
2022-12-26 15:59:26 -0800

    corrected

---
+++ DAILY REPORT/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Daily_Report_Tech_Academy
+{
+    class Program
+    {
+        static void Main()
+        {
+            //asking the console to write with the writeline the two strings.
+            Console.WriteLine("the tech academy");
+            Console.WriteLine("student daily report");
+            //console asking what your name is
+
+            //console question for your name.
+            Console.WriteLine("what is your name?");
+            string yourname = Console.ReadLine();
+            Console.WriteLine("hello" + " " + yourname);
+            //console reading answers and storing
+
+
+            //requesting input of current course.
+            Console.WriteLine("what course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("you currently are on course " + course);
+
+
+            //requesting input of page number currently on.
+            Console.WriteLine("what page number are you on:?");
+            string pagenum = Console.ReadLine();
+            int pagenumber = Convert.ToInt32(pagenum);//perfroms a caste conversion string to int
+            Console.WriteLine("you are currently on page number " + pagenumber);
+
+
+            //requesting input of help needed? boolian would be done to perform the data type statement returning true or false
+
+
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+            bool helpIsNeeded = Convert.ToBoolean(Console.ReadLine());
+
+
+            //if statement
+            if (helpIsNeeded == true)
+            {
+                Console.WriteLine("Student needs help! "); //returns our boolean statement as true help is needed
+                                                           //Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("Student does not need help! "); //returns boolean statement as false does not need help
+                Console.ReadLine();
+
+            }
+
+
+
+            //requesting input of possitive expierences
+            Console.WriteLine("Were there any posistive experiences you'd like to share? Please give specific.");
+            string experience = Console.ReadLine();
+            Console.WriteLine("Thank you for your submission of positive experiences: " + experience);
+
+            //requesting input of feedback to your instructor
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("Thank you for your feedback! " + feedback);
+
+
+            //requesting input of hrs study
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudys = Console.ReadLine();
+            //int hoursStudy = Console.ReadLine();
+            Console.WriteLine("Hours study " + hoursStudys);
+
+            //int hoursStudy =  Console.ReadLine();
+            //string final = ("today your studies accounts for" + hoursStudy);
+            // Console.WriteLine(final);
+            //Console.ReadLine("Great job, Today your total study hours are" + hoursStudy);
+
+
+            //console stating that an instructor will get back to you shortly!
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great Day!");
+            Console.ReadLine();
+            //end of program.
+
+        }
+    }
+}
---
+++ MATH AND COMPARISON OPERATOR/MATH AND COMPARISON OPERATOR/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Please press Enter to start.");
+            Console.ReadLine();
+
+            //Person 1 info
+            Console.WriteLine("Person 1");
+
+            //Person 1 hourly rate
+            Console.WriteLine("What is your hourly rate?");
+            string payRate = Console.ReadLine();
+            decimal payRate1 = Convert.ToInt32(payRate); //converting decimal to int
+            Console.WriteLine("your hourly rate is " + payRate1);
+            Console.ReadLine();
+
+            //Person 1 hours worked this week
+            Console.WriteLine("How many hours did you work this week?");
+            string hours = Console.ReadLine();
+            decimal hours1 = Convert.ToInt32(hours);
+            //int numTotal = Console.ReadLine();
+            Console.WriteLine("You worked this week " + hours1 + " hours!");
+            Console.ReadLine();
+
+            //Annual salary for Person 1
+            Console.WriteLine("Weekly salary for Person 1");
+            decimal salaryOne = payRate1 * hours1;
+            Console.WriteLine(salaryOne);
+            decimal monthlyWage = salaryOne * 4; //created a months worth of pay
+            Console.WriteLine("annual salary of person 1 is " + salaryOne * 52); //mutliply by 52 weeks in a year
+            Console.ReadLine();
+
+            //Person 2 info
+            Console.WriteLine("Person 2");
+
+            //Person 2 hourly rate
+            Console.WriteLine("What is your hourly rate?");
+            string payRate2 = Console.ReadLine();
+            decimal payRate22 = Convert.ToInt32(payRate2);
+            Console.WriteLine("your hourly rate is " + payRate22);
+            Console.ReadLine();
+
+            //Person 2 how many hours did you work this week
+            Console.WriteLine("How many hours did you work this week?");
+            string hours2 = Console.ReadLine();
+            decimal hours22 = Convert.ToInt32(hours2);
+            Console.WriteLine("This week you Worked " + hours22 + " hours!");
+            Console.ReadLine();
+
+
+            //Annual salary for person 2 
+            Console.WriteLine("Weekly salary for Person 2");
+            decimal salaryTwo = payRate22 * hours22;
+            Console.WriteLine(salaryTwo);
+            decimal monthlyWages = salaryTwo * 4;
+            Console.WriteLine("annual salary of person 2 is " + salaryTwo * 52); //mutliply by 52 weeks in a year
+            Console.ReadLine();
+
+            //Does person 1 make more money than person 2?
+            Console.WriteLine("Does Person 1 make more money than person 2?");
+            bool greaterThan = salaryOne > salaryTwo;
+            Console.WriteLine("Person 1 makes more than Person 2: " + greaterThan);
+            Console.ReadLine();
+
+
+        }
+    }
+}
