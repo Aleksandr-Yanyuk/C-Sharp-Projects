7eb14b97609b99b868591ef86a93d20fe23e0670
Alex745i <alex745i@gmail.com>
2022-12-28 16:55:57 -0800

    Package Express

--- BRANCHING/BRANCHING/Program.cs
+++ BRANCHING/BRANCHING/Program.cs
@@ -12,7 +12,7 @@ namespace BRANCHING
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
-
+            Console.ReadLine();
 
             Console.WriteLine("Please enter package weight");
             string weight = Console.ReadLine();
@@ -21,9 +21,9 @@ namespace BRANCHING
 
             if (packageWeight > 50)
             {
-                Console.WriteLine("Package is over 50, too heavy to be shipped via Package Express. Have a good day.");
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
                 Console.ReadLine();
-
+                System.Environment.Exit(1);
             }
 
             Console.WriteLine("Please enter package width");
@@ -39,13 +39,13 @@ namespace BRANCHING
             Console.WriteLine("Please enter package length");
             string length = Console.ReadLine();
             int packageLength = Convert.ToInt32(length);
-
+            Console.ReadLine();
 
             if (packageHeight + packageLength + packageWidth > 50)
             {
-                Console.WriteLine("If the package weight is over 50 or if the total dimensions are over 50 package too is big to be shipped via Package Express.");
+                Console.WriteLine("Package too big to be shipped via Package Express.");
                 Console.ReadLine();
-
+                System.Environment.Exit(1);
             }
 
             decimal sum = packageWeight * (packageHeight * packageLength * packageWidth) / 100m;
