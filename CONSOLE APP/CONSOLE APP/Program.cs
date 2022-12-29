f57496498eb93a9a55d129341f81c54e392597c2
Alex745i <alex745i@gmail.com>
2022-12-28 20:18:36 -0800

    while statement/do while statement.

--- CONSOLE APP/CONSOLE APP/Program.cs
+++ CONSOLE APP/CONSOLE APP/Program.cs
@@ -5,50 +5,65 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CONSOLE_APP
+
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // DO WHILE LOOP
+            Console.WriteLine("Whats my first name? go on, give it a guess.");
+            string guessedName = Console.ReadLine();
 
-            //this program uses switch statements cases breaks and do statements along with a while loop 
-            Console.WriteLine("Guess a number");
-            int number3 = Convert.ToInt32(Console.ReadLine());
-            bool isGuessed3 = number3 == 12;
+            bool name = guessedName == "Jon";
 
-            do //do loop
+            do
             {
-                switch (number3)
+                switch (guessedName)
                 {
-                    case 62:
-                        Console.WriteLine("You guessed 62. Try again");
-                        Console.WriteLine("Guess a number");
-                        number3 = Convert.ToInt32(Console.ReadLine());
+                    case "Jon":
+                        Console.WriteLine("\nWow, you got it right, nice job.");
+                        name = true;
                         break;
-                    case 29:
-                        Console.WriteLine("You guessed 29. Try again");
-                        Console.WriteLine("Guess another number");
-                        number3 = Convert.ToInt32(Console.ReadLine());
+
+                    case "John":
+                        Console.WriteLine("\nReally a silent h... no sir, close though.");
+                        Console.WriteLine("\nLet's try this again. Whats my first name?");
+                        guessedName = Console.ReadLine();
                         break;
-                    case 55:
-                        Console.WriteLine("You guessed 55. Try again");
-                        Console.WriteLine("Guess another number");
-                        number3 = Convert.ToInt32(Console.ReadLine());
+
+                    case "Bob":
+                        Console.WriteLine("\nNo I'm not a Bob.");
+                        Console.WriteLine("\nLet's try this again. Whats my first name?");
+                        guessedName = Console.ReadLine();
                         break;
-                    case 12:
-                        Console.WriteLine("you guessed the number 12. You are correct!");
-                        isGuessed3 = true;
-                        break;
+
                     default:
-                        Console.WriteLine("You are wrong.");
-                        //Console.WriteLine("Guess a number");
-                        //number3 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("\nOne hint, my name is 3-5 letters long.");
+                        Console.WriteLine("\nLet's try this again. Whats my first name?");
+                        guessedName = Console.ReadLine();
                         break;
                 }
             }
-            while (!isGuessed3);
-
+            while (!name);
             Console.ReadLine();
+
+
+            // WHILE LOOP
+            Console.WriteLine("Time to count to ten! Press enter to continue.");
+            Console.ReadLine();
+            int n = 0;
+
+            while (n < 11)
+            {
+                Console.WriteLine(n);
+                if (n == 4)
+                {
+                    Console.WriteLine("Half way there!");
+                }
+                n++;
+            }
+            Console.ReadKey();
         }
     }
 }
