80fa2c4b8dcc9a9af2476b6b1e9f73cdfd62b036
Alex745i <alex745i@gmail.com>
2022-12-28 18:55:39 -0800

    loop

---
+++ CONSOLE APP/CONSOLE APP/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSOLE_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62 Try Again");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guess 29. Try Again");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Tru again");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed the number 12 That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        break;
+                }
+
+            }
+            while (!isGuessed);
+
+            Console.Read();
+        }
+    }
+}
