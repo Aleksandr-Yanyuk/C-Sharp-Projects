49385063c0e17820c88ea2f4a5b222e3facdfe96
Alex745i <alex745i@gmail.com>
2022-12-29 20:36:27 -0800

    STRINGS AND INTEGERS

---
+++ CONSOLE APP STRINGS AND INTEGERS/CONSOLE APP STRINGS AND INTEGERS/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSOLE_APP_STRINGS_AND_INTEGERS
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] numbers = { 10, 8, 6 };
+                Console.WriteLine("Please enter a number:");
+                int number = Convert.ToInt32(Console.ReadLine());
+                int divide;
+
+                foreach (int items in numbers)
+                {
+                    divide = items / number;
+                    Console.WriteLine(divide);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
