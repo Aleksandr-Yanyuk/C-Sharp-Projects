b5a9ac033fb82579d26e1b16cba63d0730b53c4a
Alex745i <alex745i@gmail.com>
2022-12-30 03:29:42 -0800

    CALLING METHODS

---
+++ CALLING METHODS/CALLING METHODS/Maths.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CALLING_METHODS
+{
+    class Maths
+    {
+            public void Multi(int multiplyNum)
+            {
+                Console.WriteLine(multiplyNum * 162);
+            }
+
+            public void Adding(int additionNum)
+            {
+                Console.WriteLine(additionNum + 55);
+
+            }
+
+            public void Divide(double divideNum)
+            {
+                Console.WriteLine(divideNum / 4);
+
+            }
+        }
+    }
+
+
+
+
+
+
+
---
+++ CALLING METHODS/CALLING METHODS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CALLING_METHODS
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose a number, this program will do a muliplication, addition, and Finally Dividing the number you entered. .");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The number that you typed will be now multiplied by 162, Your answer : ");
+
+            Maths multi = new Maths();
+            multi.Multi(number);
+            Console.ReadLine();
+
+            Console.WriteLine("The number you entered plus 55, Programs answer : ");
+
+            Maths add = new Maths();
+            add.Adding(number);
+            Console.ReadLine();
+
+            Console.WriteLine("Your number divided by 4 equal  : ");
+
+            Maths divi = new Maths();
+            divi.Divide(number);
+            Console.ReadLine();
+        }
+    }
+}
