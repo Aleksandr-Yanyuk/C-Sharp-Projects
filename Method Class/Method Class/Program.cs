c4b51a6ff6bf4213a7905451d0c099b2c7dc5c2f
Alex745i <alex745i@gmail.com>
2022-12-30 19:26:16 -0800

    Class method 4

---
+++ Method Class/Method Class/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            // ESTABLISHING AN INT FOR MY OUT PARAMETER IN division CLASS
+            int num1;
+
+            Console.WriteLine("Enter a even number to be divided by two.");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            division div = new division();
+            div.divM(input);
+
+            Console.WriteLine("\nI love dividing numbers by two.");
+
+            // OVERLOADED METHOD BEFORE STATIC WAS ADDED TO CLASS
+            //div.divM(out num1);
+
+            division.divM(out num1);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method Class/Method Class/division.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class division
+    {
+        public void divM(int number1)
+        {
+            // GIVING number2 A VALUE TO BE USED IN THE MAIN PROGRAM
+            int number2 = 2;
+            int number3 = number1 / number2;
+            Console.WriteLine(number3);
+        }
+
+        public static void divM(out int number1)
+        {
+            number1 = 144;
+            int number2 = 2;
+            int number3 = number1 / number2;
+            Console.WriteLine("\nI personaly like dividing {0} by {1} because it eaqual my favorite number {2}.", number1, number2, number3);
+        }
+    }
+}
