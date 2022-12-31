a4693e442ff8de7d79282acdf0ff08b590dff954
Alex745i <alex745i@gmail.com>
2022-12-30 17:13:53 -0800

    METHOD_2

--- METHOD 2/METHOD 2/Program.cs
+++ METHOD 2/METHOD 2/Program.cs
@@ -17,16 +17,16 @@ namespace METHOD_2
                 try
                 {
 
-                    Console.WriteLine("Select a second number. Press return if you do not want a second number will be auto populated:");
-                    FirstOp.num2 = Convert.ToInt32(Console.ReadLine());
-                }
+                    Console.WriteLine("second number will be auto populated:");
+                int sum = FirstOp.MathMethod(FirstOp.num1, FirstOp.num2);
+                Console.WriteLine(sum);
+            }
                 catch (FormatException)
                 {
                     FirstOp.num2 = 782;
 
                 }
-                int sum = FirstOp.MathMethod(FirstOp.num1, FirstOp.num2);
-                Console.WriteLine(sum);
+
 
                 Console.WriteLine("Thanks for checking out the program!");
                 Console.ReadLine();
