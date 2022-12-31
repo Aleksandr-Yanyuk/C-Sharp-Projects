cd547688cca0e89e4e574ef8939e384be35d0f1b
Alex745i <alex745i@gmail.com>
2022-12-30 16:04:06 -0800

    MAIN METHOD

---
+++ MAIN METHOD/MAIN METHOD/Maths.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAIN_METHOD
+
+{
+    class Maths
+    {
+        public int one_method(int num)
+        {
+            int result = num + 5;
+            return result;
+        }
+
+        public int one_method(decimal num)
+        {
+            int result = Convert.ToInt32(num) + 10;
+            return result;
+        }
+
+        public int one_method(string num)
+        {
+            int result = Convert.ToInt32(num) + 20;
+            return result;
+        }
+    }
+
+}
---
+++ MAIN METHOD/MAIN METHOD/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAIN_METHOD
+
+    {
+
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                Maths method1 = new Maths(); //instantiating class maths
+                                        
+                Console.WriteLine("Method that takes an int and returns an int and adds 5: " + method1.one_method(12));//preset values
+
+                Console.WriteLine("Method that takes an decimal and returns an int and adds 10: " + method1.one_method(1.22m));
+
+                Console.WriteLine("Method that takes an string and returns an int and adds 20: " + method1.one_method("12"));
+
+                Console.Read();
+
+            }
+
+            public class SampleOne
+            {
+                public void IntergerMethod(int num)
+                {
+                    Decimal newValue = Convert.ToDecimal(num);
+                    Console.WriteLine(newValue);
+                    Console.ReadLine();
+                }
+
+            }
+        }
+    }
