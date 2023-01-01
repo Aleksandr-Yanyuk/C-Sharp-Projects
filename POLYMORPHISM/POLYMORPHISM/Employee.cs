dabb11cda9cffe5c844ee8b0bdc50b60703024ac
Alex745i <alex745i@gmail.com>
2022-12-31 22:07:27 -0800

    POLYMORPHISM

---
+++ POLYMORPHISM/POLYMORPHISM/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POLYMORPHISM
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Employee: ");
+            base.SayName();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("\nCurrent satic: \nHas quit");
+        }
+    }
+}
