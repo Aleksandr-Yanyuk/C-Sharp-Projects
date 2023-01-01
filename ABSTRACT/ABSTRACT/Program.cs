9ea7e64df3027e1627adb57bde2fa67a3ec8c64a
Alex745i <alex745i@gmail.com>
2022-12-31 19:16:43 -0800

    ABSTRACT

---
+++ ABSTRACT/ABSTRACT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABSTRACT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+
+            IQuittable quit = new Employee();
+            quit.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
