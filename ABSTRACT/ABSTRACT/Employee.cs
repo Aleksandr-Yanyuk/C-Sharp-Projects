d4ba5ecbbd969c08d14ebe2b77218e9a3c99d838
Alex745i <alex745i@gmail.com>
2023-01-01 17:09:52 -0800

    ABSTRACT

--- ABSTRACT/ABSTRACT/Employee.cs
+++ ABSTRACT/ABSTRACT/Employee.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace ABSTRACT
 {
+    //Create another class called Employee and have it inherit from the Person class.
     public class Employee : Person
     {
+        //Implement the SayName() method inside of the Employee class.
         public override void SayName()
         {
             Console.WriteLine("Employee: ");
--- ABSTRACT/ABSTRACT/Program.cs
+++ ABSTRACT/ABSTRACT/Program.cs
@@ -10,11 +10,10 @@ namespace ABSTRACT
     {
         static void Main(string[] args)
         {
+            //Inside the Main() method, instantiate an Employee object with
+            //firstName “Sample” and lastName “Student”. Call the SayName() method on the object.
             Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
             employee.SayName();
-
-
-
             Console.ReadLine();
         }
     }
