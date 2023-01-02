e46cd1aafbad2c7cccba804a1e58840821bf8815
Alex745i <alex745i@gmail.com>
2023-01-01 21:48:12 -0800

    OPERATORS

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+
+{
+    public class Employee
+    {
+        //Created an Employee class with Id, FirstName and LastName properties. 
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //overloaded operator pair to compare employee ids
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;
+
+        }
+    }
+}
--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -10,38 +10,15 @@ namespace Operators
     {
         static void Main(string[] args)
         {
-            Employee<string> sampleEmployee = new Employee<string>();
-            sampleEmployee.FirstName = "Sample";
-            sampleEmployee.LastName = "Student";
-            sampleEmployee.Id = 007;
-            sampleEmployee.things = new List<string>() { "Boost Software", "Downpipes", "Turbo", "Supercharger", "Intakes" };
-            sampleEmployee.SayName();
-            Console.WriteLine("Employee ID: " + Convert.ToString(sampleEmployee.Id));
-            Console.WriteLine("First Employee's Request to make a highly boosted car!:");
-            foreach (string tool in sampleEmployee.things)
-            {
-                Console.WriteLine(tool);
-            }
+            //instantiated 2 new employee objects
+            Employee emp1 = new Employee();
+            Employee emp2 = new Employee();
+            emp1.Id = 1;
+            emp2.Id = 2;
 
-            Employee<string> employeeTwo = new Employee<string>();
-            employeeTwo.Id = 700;
-            bool sameIDs = employeeTwo == sampleEmployee;
-            Console.WriteLine("sampleEmployee and employeeTwo have identical IDs: " + Convert.ToString(sameIDs));
-            Employee<int> employeeThree = new Employee<int>();
-            employeeThree.things = new List<int>() { 1, 7, 4 };
-            Console.WriteLine("Employee Three's List of Favorite Numbers:");
-            foreach (int num in employeeThree.things)
-            {
-                Console.WriteLine(Convert.ToString(num));
-            }
-
-            sampleEmployee.Quit();
-
-            IQuittable quitTime = new Employee<double>();
-
-            Console.WriteLine("Hope you enjoyed your stay here with this program...");
-            quitTime.Quit();
-
+            //compared id of employees
+            Console.WriteLine(emp1 == emp2);
+            Console.ReadLine();
         }
     }
 }
