13ab4af3113b6ee198bd5bfb92c38c29889475eb
Alex745i <alex745i@gmail.com>
2023-01-03 17:34:17 -0800

    DATE/TIME

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiates new DateTime object
+            DateTime now = DateTime.Now;
+
+            //shows only time of DateTime
+            Console.WriteLine(now.ToString("hh:mm tt"));
+            Console.WriteLine("Please enter a number.");
+            //Converts user input to data type of double
+            Double x = Convert.ToDouble(Console.ReadLine());
+            //adds user input to current time, instantiating it to later
+            DateTime later = DateTime.Now.AddHours(x);
+            //writes only the time of the DateTime added from user input
+            Console.WriteLine(later.ToString("hh:mm tt"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TwentyOne/TwentyOne/TwentyOneGame.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TwentyOne
+{
+    public class TwentyOneGame : Game, IWalkAway
+    {
+        public TwentyOneDealer Dealer { get; set; }
+        //override allows us to implement the inherited abstract method from the Game class
+        public override void Play()
+        {
+            //instantiates new dealer object
+            Dealer = new TwentyOneDealer();
+            foreach (Player player in Players)
+            {
+                player.Hand = new List<Card>();
+                player.Stay = false;
+            }
+            Dealer.Hand = new List<Card>();
+            Dealer.Stay = false;
+            Dealer.Deck = new Deck();
+            console.WriteLine("Place your bet!");
+
+            foreach (Player player in Players)
+            {
+                int bet = Convert.ToInt32(Console.ReadLine());
+                bool successfullyBet = player.Bet(bet);
+                if (!successfullyBet)
+                {
+                    return;
+                }
+                Bets[player] = bet;
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Dealing...");
+                foreach (Player player in Players)
+                {
+                    Console.Write("{0}: ", player.Name);
+                    Dealer.Deal(player.Hand);
+                    if (i == 1)
+                    {
+                        bool blackJack = TwentyOneRules.CheckForBlackJack(player.Hand);
+                        if (blackJack)
+                        {
+                            Console.WriteLine("Blackjack! {0} wins {1}", player.Name, Bets[player]);
+                            player.Balance += Convert.ToInt32((Bets[player] * 1.5) + Bets[player]);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+            public override void ListPlayers()
+        {
+            Console.WriteLine("21 Players:");
+            base.ListPlayers();
+        }
+        public void WalkAway(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
