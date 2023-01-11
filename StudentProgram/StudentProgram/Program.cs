17c51a96d8b6d02bae8ee8ec0011f2bef78b6923
Alex745i <alex745i@gmail.com>
2023-01-10 22:51:49 -0800

    StudentProgram

---
+++ StudentProgram/StudentProgram/Program.cs
@@ -0,0 +1,9 @@
+﻿using StudentProgram;
+
+using (var ctx = new SchoolContext())
+{
+    var stud = new Student() { StudentName = "Bill" };
+
+    ctx.Students.Add(stud);
+    ctx.SaveChanges();
+}
---
+++ StudentProgram/StudentProgram/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentProgram
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
